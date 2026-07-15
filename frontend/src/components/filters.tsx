a9a9c542c8ffcebf551d640a64477c12849cb662
w-is-h <w.kraljevic@gmail.com>
2026-07-15 17:59:07 +0100

    filters: size the dropdown to content, capped
    w-fit up to max-w-64; long usernames wrap inside the cap.
    
    -- zk, wren

--- frontend/src/components/filters.tsx
+++ frontend/src/components/filters.tsx
@@ -147,7 +147,7 @@ export function FilterMenu({ done = true, archived = false, empty = false, users
       >
         <ListFilter className="size-4" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="min-w-64">
+      <DropdownMenuContent align="end" className="w-fit max-w-64">
         {done && (
           <DropdownMenuCheckboxItem
             checked={filters.showDone}
